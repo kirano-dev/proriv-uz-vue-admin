9bfc768ba915e61546c72fbd243059e8ceb1362f
kiranouz <developer@kirano.uz>
2025-10-18 13:36:33 +0500

    !

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import ru from 'element-plus/dist/locale/ru.mjs'
+import '@/assets/styles/global.scss'
 
 createApp(App)
   .use(createPinia())
