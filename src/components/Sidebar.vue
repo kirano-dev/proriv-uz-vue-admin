6a8fe277006c8f04c1ef802d0f235735eee5a9a2
kiranouz <developer@kirano.uz>
2025-11-06 15:30:02 +0500

    !

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <aside class="sidebar">
+    <div class="sidebar__logo">GROW UP</div>
+    <nav class="sidebar__nav">
+      <ul>
+        <li class="sidebar__nav-item">
+          <a href="#" class="sidebar__nav-link">
+            <img src="" alt="">
+            <span>Лидерская программа</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<style scoped>
+
+</style>
