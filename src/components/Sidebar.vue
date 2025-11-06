738d08848b96d77897e0440c4b21ac74fdb14e4c
kiranouz <developer@kirano.uz>
2025-11-06 18:38:25 +0500

    !

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,10 +1,21 @@
 <script setup>
 import { ref } from 'vue'
-import ChartSvg from '@/assets/icons/lp.svg'
+import lpSvg from '@/assets/icons/lp.svg'
+import evolutionSvg from '@/assets/icons/evolution.svg'
+import prorivSvg from '@/assets/icons/proriv.svg'
+import clubSvg from '@/assets/icons/club.svg'
+import leadsSvg from '@/assets/icons/leads.svg'
+import accessSvg from '@/assets/icons/access.svg'
+import eventsSvg from '@/assets/icons/events.svg'
 
 const items = ref([
-  { to: '/lp', label: 'Лидерская программа', svg: ChartSvg },
-  { to: '/courses', label: 'Курсы', svg: ChartSvg }
+  { to: '/lp', label: 'Лидерская программа', svg: lpSvg },
+  { to: '/evolution', label: 'Эволюция', svg: evolutionSvg },
+  { to: '/proriv', label: 'Прорыв', svg: prorivSvg },
+  { to: '/leads', label: 'Заявки', svg: leadsSvg },
+  { to: '/access', label: 'Открыть доступ', svg: accessSvg },
+  { to: '/events', label: 'События', svg: eventsSvg },
+  { to: '/club', label: 'GrowUp Club', svg: clubSvg },
 ])
 </script>
 
