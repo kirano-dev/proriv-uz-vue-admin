acff90c88a9db25b087b6c34118282d4377d749f
kirano-dev <developer@kirano.uz>
2025-12-24 10:43:48 +0500

    !

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.3:8000/api/v1/',
+  baseURL: 'http://192.168.1.13:8000/api/v1/',
 });
 
 // при старте приложения — если в localStorage есть токен, подставляем его
